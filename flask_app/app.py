d97617e4589bb5aba2db61178dee2ea5d4936561
Jan R <jan@app.c.ad4r-2020.internal>
2020-07-09 14:42:26 +0000

    update

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,20 +3,48 @@ from flask import request
 from elasticsearch import Elasticsearch
 import json
 from flask_cors import CORS, cross_origin
+import pandas as pd
+import time
+import logging
 
+time.sleep(12)
 
-# es = Elasticsearch('http://localhost:9200') #es.indices.delete('my_index')
+es = Elasticsearch('elasticsearch:9200') #es.indices.delete('my_index')
 
-# while True:
+while True:
+    try:
+        es.search(index="")
+        break
+    except (
+        elasticsearch.exceptions.ConnectionError,
+        elasticsearch.exceptions.TransportError
+    ):
+        time.sleep(1)
+
+# def seed():
 #     try:
-#         es.search(index="")
-#         break
-#     except (
-#         elasticsearch.exceptions.ConnectionError,
-#         elasticsearch.exceptions.TransportError
-#     ):
-#         time.sleep(1)
+#         es.indices.delete('cvpr')
+#     except:
+#         print("Index already deleted.")
 
+#     df = pd.read_csv("/app/cvpr_data_preprocessed.csv")
+
+#     print("in seed data")
+
+#     for index,row in df.iterrows():
+#         title = row["title"]
+#         abstract = row["abstract"]
+#         authors = row["authors"]
+#         authors = "; ".join(authors.split(";"))
+#         institutions = row["institutions"]
+#         institutions = "; ".join(institutions.split(";"))
+#         url = row["url"]
+#         es.index(index='cvpr', id=index, body={'title': title, 'abstract': abstract, 'authors': authors, 'institutions': institutions, 'url': url})
+#     # print(counter)
+
+#     print("Elastic Search successfully seeded with index: cvpr")
+
+# seed()
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -47,7 +75,12 @@ def search_all():
 
     if len(fields) == 0:
         fields = [""]
-    print(fields)
+    print(query_string_formatted)
+    app.logger.debug("####### NEW QUERY ###########")
+    app.logger.debug(f'Query string: {query_string}')
+    app.logger.debug(f'Query location: {query_location}')
+    
+
     query = {
         "query":{
             "bool":{
@@ -62,13 +95,30 @@ def search_all():
             }
         }
     }
+    
+    # query = {
+    #     "query": {
+    #         "multi_match" : {
+    #         "query" : query_string,
+    #         "fields" : fields,
+    #         "type": "phrase"
+    #         }
+    #     }
+    # }
 
     result = es.search(index='cvpr', body=query, size=1800)
     result_formatted = []
     for i in result["hits"]["hits"]:
         dict_tmp = {"title": i["_source"]["title"], "abstract": i["_source"]["abstract"], "institutions": i["_source"]["institutions"], "authors": i["_source"]["authors"], "url": i["_source"]["url"] }
         result_formatted.append(dict_tmp)
+    app.logger.debug(len(result_formatted))
+    app.logger.debug("##########  ###########")
+    dict_result = {"query": query_string, "result": result_formatted}
     return json.dumps(result_formatted)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+else:
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
